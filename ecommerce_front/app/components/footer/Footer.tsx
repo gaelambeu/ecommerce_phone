1565b6a94c6a12f51388befd9d308740f531bfe3
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-09 20:49:06 +0000

    footer

--- ecommerce_front/app/components/footer/Footer.tsx
+++ ecommerce_front/app/components/footer/Footer.tsx
@@ -8,10 +8,28 @@ const Footer = () => {
             <Container>
                 <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
                     <FooterList>
-                        <h3>Shop Categories</h3>
+                        <h3 className="text-base font-bold mb-2">Shop Categories</h3>
                         <Link href="#">Phones</Link>
+                        <Link href="#">Laptops</Link>
+                        <Link href="#">Desktops</Link>
+                        <Link href="#">Watches</Link>
+                        <Link href="#">Tv5</Link>
+                        <Link href="#">Accessories</Link>
                     </FooterList>
-
+                    <FooterList>
+                        <h3 className="text-base font-bold mb-2">Customer Service</h3>
+                        <Link href="#">Contact US</Link>
+                        <Link href="#">Shipping Policy</Link>
+                        <Link href="#">Returns & Exchanges</Link>
+                        <Link href="#">FAQs</Link>
+                    </FooterList>
+                    <div className="w-full md:w-1/3 mb-6 md:mb-0">
+                        <h3 className="text-base font-bold mb-2">About Us</h3>
+                        <p className="mb-2">At our electronics store. We are dedicted to providing the latest
+                        and greatest devices and accessories to our customers. With a wide
+                        selection of phones. TVs. laptops. watches. and accessories.</p>
+                        <p>&copy: {}</p>
+                    </div>
                 </div>
             </Container>
         </footer>
