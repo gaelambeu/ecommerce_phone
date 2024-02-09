87a3007e59680232c1c793bfb914252ea25f7d14
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-09 18:42:55 +0000

    short

--- ecommerce_front/app/components/nav/NavBar.tsx
+++ ecommerce_front/app/components/nav/NavBar.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import Container from "../Container";
+
 const NavBar = () => {
     return ( 
         <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
-            NavBar
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    <div className="flex items-center justify-between gap-3 md-gap-0">
+                        <Link href="/">E-Shop</Link>
+                        <div className="hidden md:block">Search</div>
+                        <div className="flex items-center gap-8 md:gap-12">
+                            <div>CartCount</div>
+                            <div>UserMenu</div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
         </div>
     );
 }
