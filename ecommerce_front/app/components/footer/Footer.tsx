42ef9234f121503271d04fbb9fb0dfdc39f0a6a4
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-09 19:36:33 +0000

    short

--- ecommerce_front/app/components/footer/Footer.tsx
+++ ecommerce_front/app/components/footer/Footer.tsx
@@ -1,6 +1,18 @@
+import Container from "../Container";
+import FooterList from "./FooterList";
+
 const Footer = () => {
     return (  
-        <div>Footer</div>
+        <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+            <Container>
+                <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+                    <FooterList>
+                        
+                    </FooterList>
+
+                </div>
+            </Container>
+        </footer>
     );
 }
  
