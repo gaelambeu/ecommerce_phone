859b55cd3709af97d1ff6679d6f901c1e5c32c93
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-09 13:26:04 +0000

    short

--- ecommerce_admin/app/page.js
+++ ecommerce_admin/app/page.js
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
-    <div className="bg-blue-900 w-screen h-screen flex">
-      <div>
-        
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
       </div>
-      <button className="bg-white p-2">Login with Google</button>
     </div>
   );
 }
