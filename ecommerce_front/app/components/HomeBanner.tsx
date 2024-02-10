728b4bb6545717f7276fe5045900e2ce7f48acc0
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-10 00:32:17 +0000

    short

---
+++ ecommerce_front/app/components/HomeBanner.tsx
@@ -0,0 +1,12 @@
+const HomeBanner = () => {
+    return (  
+        <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
+            <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
+ 
+export default HomeBanner;
