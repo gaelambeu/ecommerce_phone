9e479f7f5c31a90f90bf0ccde1437bfdecf400ca
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-10 00:40:06 +0000

    short

--- ecommerce_front/app/components/HomeBanner.tsx
+++ ecommerce_front/app/components/HomeBanner.tsx
@@ -2,7 +2,11 @@ const HomeBanner = () => {
     return (  
         <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
             <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
-                <div></div>
+                <div>
+                    <h1>Summer Sale</h1>
+                    <p>Enjoy discounts on selected items</p>
+                    <p>GET 50% OFF</p>
+                </div>
                 <div></div>
             </div>
         </div>
