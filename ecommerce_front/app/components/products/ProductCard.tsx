c248c4136bd7497dcb6a349221367d11bca10d33
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-12 16:54:18 +0000

    short

--- ecommerce_front/app/components/products/ProductCard.tsx
+++ ecommerce_front/app/components/products/ProductCard.tsx
@@ -11,10 +11,12 @@ const ProductCard: React.FC<ProductCardProps> = ({data}) => {
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105
         text-center text-sm">
             <div className="flex flex-col items-center w-full gap-1">
-                <div>
+                <div className="aspect-square overflow-hidden relative w-full">
                     <Image 
                         fill
-                        className=""
+                        src={data.images[0].image}
+                        alt={data.name}
+                        className="w-full h-full object-contain"
                     />
                 </div>
                 <div></div>
