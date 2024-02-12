a7260e401de823e375bfe1e3a41b1497c2fe268b
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-12 16:22:36 +0000

    short

--- ecommerce_front/app/page.tsx
+++ ecommerce_front/app/page.tsx
@@ -1,6 +1,7 @@
 import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import { truncateText } from "@/utils/truncateText";
 
 export default function Home() {
   return (
@@ -9,9 +10,9 @@ export default function Home() {
           <div>
             <HomeBanner />
           </div>
-          <div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap:8">
             {products.map((product: any) => {
-              return <div>{product.name}</div>
+              return <div>{truncateText(product.name)}</div>
             })}
           </div>
       </Container>
