5bc893dea49c0b9fdcd9f183a09cc928a3d18201
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-15 12:54:58 +0000

    short

--- ecommerce_front/app/product/[productId]/ProductDetails.tsx
+++ ecommerce_front/app/product/[productId]/ProductDetails.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 interface ProductDetailProps{
     product: any
 }
 
-const ProductDetails:React.FC<ProductDetailProps> = () => {
+const ProductDetails:React.FC<ProductDetailProps> = ({ product }) => {
     return (  
-        <div>
-            Product
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+            <div>
+                Images
+            </div>
+            <div>details</div>
         </div>
     );
 }
