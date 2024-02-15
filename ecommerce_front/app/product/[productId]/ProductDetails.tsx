db2f66678c8a467d1acf8aabe2ecefe4e4953843
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-15 13:05:00 +0000

    short

--- ecommerce_front/app/product/[productId]/ProductDetails.tsx
+++ ecommerce_front/app/product/[productId]/ProductDetails.tsx
@@ -1,16 +1,32 @@
 'use client';
 
+import { Rating } from "@mui/material";
+
 interface ProductDetailProps{
     product: any
 }
 
 const ProductDetails:React.FC<ProductDetailProps> = ({ product }) => {
+
+    const productRating = product.reviews.reduce((acc: number, item: any) => 
+    item.rating + acc, 0) / 
+    product.reviews.length;
+
     return (  
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
             <div>
                 Images
             </div>
-            <div className="text-3xl font-medium text-slate-700">{product.name}</div>
+            <div>
+                <h2 className="text-3xl font-medium text-slate-700">
+                    {product.name}
+                </h2>
+                <div>
+                    <Rating value={productRating} readOnly />
+                    
+                </div>
+            </div>
+            
         </div>
     );
 }
