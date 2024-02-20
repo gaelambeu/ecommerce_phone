7b32cb36c7b78cf1babbfd0018172553ad821253
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-20 15:31:14 +0000

    short

---
+++ ecommerce_front/app/components/products/SetColor.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { SelectedImgType } from "@/app/product/[productId]/ProductDetails"; }
+
+interface SetColorProps{
+    images: SelectedImgType[],
+    
+}
+
+const SetColor = () => {
+    return (  
+        <div></div>
+    );
+}
+ 
+export default SetColor;
